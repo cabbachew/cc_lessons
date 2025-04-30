79d50869a56d7ea8bc65cadcd8c968ffcadf70fd
Daniel Oh <daniel@curiouscardinals.com>
2025-04-30 11:55:24 -0700

    add tab navigation to engagement page

--- app/engagement/page.tsx
+++ app/engagement/page.tsx
@@ -1,11 +1,29 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+"use client";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 import { PageLayout } from "../components/PageLayout";
 import { apercu, apercuMono } from "../fonts";
 import { BreakpointBadge } from "../components/BreakpointBadge";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function EngagementPage() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index);
+  };
+
   return (
     <PageLayout>
       <Box position="relative">
@@ -50,6 +68,168 @@ export default function EngagementPage() {
               Quintin Leong&apos;s Home Improvement Passion Project
             </Heading>
           </Flex>
+
+          <Box
+            overflowX="auto"
+            sx={{
+              "&::-webkit-scrollbar": {
+                display: "none",
+              },
+              scrollbarWidth: "none",
+            }}
+          >
+            <Tabs
+              mt={6}
+              index={tabIndex}
+              onChange={handleTabsChange}
+              variant="unstyled"
+              colorScheme="yellow"
+              size="sm"
+            >
+              <TabList
+                borderBottomWidth="1px"
+                borderColor="gray.200"
+                minW="max-content"
+              >
+                <Tab
+                  className={apercuMono.className}
+                  fontSize={{ base: "10px", md: "xs" }}
+                  fontWeight="medium"
+                  letterSpacing="1px"
+                  px={{ base: 2, sm: 3, md: 4 }}
+                  py={3}
+                  mr={{ base: 1, md: 2 }}
+                  color="#6b7280"
+                  position="relative"
+                  _selected={{
+                    color: "#111827",
+                    fontWeight: "bold",
+                    _after: {
+                      content: '""',
+                      position: "absolute",
+                      bottom: "-1px",
+                      left: 0,
+                      right: 0,
+                      height: "2px",
+                      bg: "#e3c757",
+                    },
+                  }}
+                >
+                  OVERVIEW
+                </Tab>
+                <Tab
+                  className={apercuMono.className}
+                  fontSize={{ base: "10px", md: "xs" }}
+                  fontWeight="medium"
+                  letterSpacing="1px"
+                  px={{ base: 2, sm: 3, md: 4 }}
+                  py={3}
+                  mr={{ base: 1, md: 2 }}
+                  color="#6b7280"
+                  position="relative"
+                  _selected={{
+                    color: "#111827",
+                    fontWeight: "bold",
+                    _after: {
+                      content: '""',
+                      position: "absolute",
+                      bottom: "-1px",
+                      left: 0,
+                      right: 0,
+                      height: "2px",
+                      bg: "#e3c757",
+                    },
+                  }}
+                >
+                  LEARNING PLAN
+                </Tab>
+                <Tab
+                  className={apercuMono.className}
+                  fontSize={{ base: "10px", md: "xs" }}
+                  fontWeight="medium"
+                  letterSpacing="1px"
+                  px={{ base: 2, sm: 3, md: 4 }}
+                  py={3}
+                  mr={{ base: 1, md: 2 }}
+                  color="#6b7280"
+                  position="relative"
+                  _selected={{
+                    color: "#111827",
+                    fontWeight: "bold",
+                    _after: {
+                      content: '""',
+                      position: "absolute",
+                      bottom: "-1px",
+                      left: 0,
+                      right: 0,
+                      height: "2px",
+                      bg: "#e3c757",
+                    },
+                  }}
+                >
+                  MILESTONES
+                </Tab>
+                <Tab
+                  className={apercuMono.className}
+                  fontSize={{ base: "10px", md: "xs" }}
+                  fontWeight="medium"
+                  letterSpacing="1px"
+                  px={{ base: 2, sm: 3, md: 4 }}
+                  py={3}
+                  mr={{ base: 1, md: 2 }}
+                  color="#6b7280"
+                  position="relative"
+                  _selected={{
+                    color: "#111827",
+                    fontWeight: "bold",
+                    _after: {
+                      content: '""',
+                      position: "absolute",
+                      bottom: "-1px",
+                      left: 0,
+                      right: 0,
+                      height: "2px",
+                      bg: "#e3c757",
+                    },
+                  }}
+                >
+                  ARTIFACTS
+                </Tab>
+                <Tab
+                  className={apercuMono.className}
+                  fontSize={{ base: "10px", md: "xs" }}
+                  fontWeight="medium"
+                  letterSpacing="1px"
+                  px={{ base: 2, sm: 3, md: 4 }}
+                  py={3}
+                  color="#6b7280"
+                  position="relative"
+                  _selected={{
+                    color: "#111827",
+                    fontWeight: "bold",
+                    _after: {
+                      content: '""',
+                      position: "absolute",
+                      bottom: "-1px",
+                      left: 0,
+                      right: 0,
+                      height: "2px",
+                      bg: "#e3c757",
+                    },
+                  }}
+                >
+                  HISTORY
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel p={4}></TabPanel>
+                <TabPanel p={4}></TabPanel>
+                <TabPanel p={4}></TabPanel>
+                <TabPanel p={4}></TabPanel>
+                <TabPanel p={4}></TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
         </Box>
       </Box>
     </PageLayout>
