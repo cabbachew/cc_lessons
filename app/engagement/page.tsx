7b1d17df702da5813e18019b6935806dc9f1399a
Daniel Oh <daniel@curiouscardinals.com>
2025-05-02 16:22:12 -0700

    implement key dates grid layout with sample dates

--- app/engagement/page.tsx
+++ app/engagement/page.tsx
@@ -1044,8 +1044,8 @@ export default function EngagementPage() {
                             flex="1"
                             bg="white"
                             p={6}
-                            borderWidth="1px"
-                            borderColor="gray.200"
+                            // borderWidth="1px"
+                            // borderColor="gray.200"
                           >
                             <Text
                               className={apercu.className}
@@ -1056,14 +1056,133 @@ export default function EngagementPage() {
                             >
                               Key Dates
                             </Text>
-                            <Text
-                              className={inter.className}
-                              fontSize="sm"
-                              color="#4B5563"
-                              mb={4}
-                            >
-                              Tentative space to list key dates and deadlines.
-                            </Text>
+
+                            <Box>
+                              <Box
+                                display="grid"
+                                gridTemplateColumns="1fr 3fr"
+                                gap={4}
+                              >
+                                {/* First Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  OCT 11, 2024
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Math test
+                                </Text>
+
+                                {/* Second Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  DEC 3, 2024
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Completed homework before tutoring session
+                                </Text>
+
+                                {/* Third Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  DEC 10, 2024
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Math exam
+                                </Text>
+
+                                {/* Fourth Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  DEC 16, 2024
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Winter break begins
+                                </Text>
+
+                                {/* Fifth Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  JAN 6, 2025
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Winter break ends
+                                </Text>
+
+                                {/* Sixth Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  FEB 5, 2025
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Spanish test on preterite and imperfect tenses
+                                </Text>
+
+                                {/* Seventh Row */}
+                                <Text
+                                  className={apercuMono.className}
+                                  fontSize="xs"
+                                  color="#6b7280"
+                                  fontWeight="medium"
+                                >
+                                  MAR 10, 2025
+                                </Text>
+                                <Text
+                                  className={inter.className}
+                                  fontSize="sm"
+                                  color="#4B5563"
+                                >
+                                  Spanish test on verb conjugations
+                                </Text>
+                              </Box>
+                            </Box>
                           </Box>
                         </Flex>
                       </Box>
